5ff3e8b822340845927f1e6347d03b7917b44ac7
Hyeongjin Kim <kimjin5537@daum.net>
2023-06-12 22:24:40 +0900

    Add: 기본 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// base
+import React from "react";
+
+// packages
+import styled from "styled-components";
+
+// etc
+import "./App.css";
+import doge from "./assets/images/doge.gif";
+
+const StyledApp = styled.div<{ doge: string }>`
+  width: 48rem;
+  min-height: 48rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: url(${(props) => props.doge});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  .title {
+    text-align: center;
+    padding: 4rem 0;
+    font-size: 4.2rem;
+    font-weight: 600;
+    margin-bottom: 3rem;
+  }
+
+  .content {
+    width: 30rem;
+    height: 10rem;
+    margin-top: 10rem;
+    text-align: center;
+    font-size: 2rem;
+    background: rgba(255, 255, 255, 0.5);
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp className="App" doge={doge}>
+      <div className="title">COIN</div>
+      <div className="content">검색 및 가격</div>
+    </StyledApp>
   );
 }
 
